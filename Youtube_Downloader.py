c3383792da431f7bbc7ac14c2a71ec0cf7b11b07
26th-Official <you@example.com>
2023-02-18 09:37:12 +0530

    Changed Quality Settings

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,6 +1,6 @@
 from pytube import Playlist
 from pytube.helpers import safe_filename 
-p = Playlist('https://youtube.com/playlist?list=PLnIP3P5w0BIEq5OaRAy5nzqOx3VkAliVT')
+p = Playlist('https://youtube.com/playlist?list=PLbjn7kaP877u1sX4zl081V8jUeSHDY18G')
 
 count = 1
 error = []
@@ -8,7 +8,7 @@ for video in p.videos:
     try:
         print(f'Downloading: {count}')
         name = safe_filename(video.title)
-        video.streams.filter(res="240p").first().download(output_path="C:\\Users\\26tho\\Desktop\\OC\\",filename=name[:30]+".mp4",filename_prefix=f"{count}_")
+        video.streams.filter(progressive=True, file_extension='mp4', res="720p").first().download(output_path=r"D:\Tutorial\Blender Drivers",filename=name[:30]+".mp4",filename_prefix=f"{count}_")
         count+=1
     except:
         error.append(name)
